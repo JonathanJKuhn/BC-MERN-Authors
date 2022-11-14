d65a063d71f1f933860f09a49fefb3aeeb6de1ae
Jonathan Kuhn <jkuhn96@gmail.com>
2022-11-14 12:48:52 -0500

    Fixed update validation for API

--- server/controllers/author.controller.js
+++ server/controllers/author.controller.js
@@ -7,37 +7,37 @@ module.exports.index = (req, res) => {
 
 module.exports.createAuthor = (req, res) => {
   Author.create(req.body)
-    .then((author) => res.json(author))
-    .catch((err) => res.json(err));
+    .then((author) => res.status(201).json(author))
+    .catch((err) => res.status(400).json(err));
 };
 
 module.exports.getAllAuthors = (req, res) => {
-  Author.find({})
-    .then((authors) => {
-      res.json(authors);
-    })
-    .catch((err) => res.json(err));
+  Author.find()
+    .sort({ name: 1 })
+    .then((authors) => res.json(authors))
+    .catch((err) => res.status(400).json(err));
 };
 
 module.exports.getAuthor = (req, res) => {
   Author.findOne({ _id: req.params.id })
     .then((author) => res.json(author))
     .catch((err) => {
-      err[
-        "message"
-      ] = `Could not find author with given id, ErrorMsg: ${err.message}`;
+      err["message"] = "Could not find author with given id";
       res.status(404).json(err);
     });
 };
 
 module.exports.updateAuthor = (req, res) => {
-  Author.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+  Author.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  })
     .then((updatedAuthor) => res.json(updatedAuthor))
-    .catch((err) => res.json(err));
+    .catch((err) => res.status(400).json(err));
 };
 
 module.exports.deleteAuthor = (req, res) => {
   Author.deleteOne({ _id: req.params.id })
     .then((deleteConfirmation) => res.json(deleteConfirmation))
-    .catch((err) => res.json(err));
+    .catch((err) => res.status(400).json(err));
 };
--- server/models/author.model.js
+++ server/models/author.model.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const AuthorSchema = new mongoose.Schema(
   {
-    name: { type: String },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      minLength: [3, "Name must be at least 3 characters long"],
+    },
   },
   { timestamps: true }
 );
