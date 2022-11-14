a3fa94a809f8f03c471d2e57cef4d491109bbbd6
Jonathan Kuhn <jkuhn96@gmail.com>
2022-11-14 11:06:57 -0500

    Added basic CRUD to API

--- server/controllers/author.controller.js
+++ server/controllers/author.controller.js
@@ -4,3 +4,40 @@ module.exports.index = (req, res) => {
     message: "API Index",
   });
 };
+
+module.exports.createAuthor = (req, res) => {
+  Author.create(req.body)
+    .then((author) => res.json(author))
+    .catch((err) => res.json(err));
+};
+
+module.exports.getAllAuthors = (req, res) => {
+  Author.find({})
+    .then((authors) => {
+      res.json(authors);
+    })
+    .catch((err) => res.json(err));
+};
+
+module.exports.getAuthor = (req, res) => {
+  Author.findOne({ _id: req.params.id })
+    .then((author) => res.json(author))
+    .catch((err) => {
+      err[
+        "message"
+      ] = `Could not find author with given id, ErrorMsg: ${err.message}`;
+      res.status(404).json(err);
+    });
+};
+
+module.exports.updateAuthor = (req, res) => {
+  Author.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then((updatedAuthor) => res.json(updatedAuthor))
+    .catch((err) => res.json(err));
+};
+
+module.exports.deleteAuthor = (req, res) => {
+  Author.deleteOne({ _id: req.params.id })
+    .then((deleteConfirmation) => res.json(deleteConfirmation))
+    .catch((err) => res.json(err));
+};
